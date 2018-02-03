b2245ba0b299043d8d304b5268364ec48560bd2c
Harshad Ranganathan <RanganathanH@DNB.com>
2018-02-03 15:38:31 +0000

    redux plumbing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './component/app';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+const createStoreWithMiddleWare = applyMiddleware()(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleWare(reducers)}>
+        <App />
+    </Provider>
+    , document.querySelector('.container'));
