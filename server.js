b67278fcdb0e5a89d613e596551d3e750841d70d
Harshad Ranganathan <RanganathanH@DNB.com>
2018-02-05 09:09:13 +0000

    Added https proxy to avoid CORS issue

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const proxy = require('express-http-proxy');
+const proxy = require('http-proxy-middleware');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
-app.use('/gnib-proxy', proxy('burghquayregistrationoffice.inis.gov.ie'));
+app.use('/gnib-proxy', proxy({
+    pathRewrite: {
+        '^/gnib-proxy/': '/'
+    },
+    target: 'https://burghquayregistrationoffice.inis.gov.ie',
+    secure: false
+}));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'));
