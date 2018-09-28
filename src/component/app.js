d52d0db6419216629f6e0d4582bcfeaff0758f05
Harshad Ranganathan <RanganathanH@DNB.com>
2018-09-28 20:31:39 +0100

    Paypal donate added

--- src/component/app.js
+++ src/component/app.js
@@ -5,6 +5,7 @@ import Disqus from '../component/disqus';
 const App = () => {
     return(
         <div>
+            <a href="https://www.paypal.me/harshadranganathan" target="_blank"><img className="float-right" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"/></a><br />
             <h3 className="text-center">GNIB (IRP) &amp; Re-Entry Visa Appointments</h3><br />
             <button type="button" className="btn float-right" onClick={() => window.location.href='https://m.me/dbei-bot'}>
                 <i className="fab fa-facebook-messenger fa-w-14 fa-1x"></i><span className="pl-2">Stamp 4 Notification</span>
