bc105b34f1503d313d316c4244fbdb369a05b06a
Harshad Ranganathan <RanganathanH@DNB.com>
2018-11-17 20:08:01 +0000

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
   npm run build
-  rsync -r --delete --quiet $TRAVIS_BUILD_DIR/public travis@206.189.245.160:gnib-visa-app/public
+  rsync -rq --delete --rsync-path="mkdir -p gnib-visa-app/public && rsync" $TRAVIS_BUILD_DIR/public travis@206.189.245.160:gnib-visa-app/public
 else
   echo "Not deploying, since this branch isn't master."
 fi
