cf4aa527372789c49c1d5f670b3127587fc2d8b1
Harshad Ranganathan <RanganathanH@DNB.com>
2018-11-17 19:08:42 +0000

    Added travis config

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add
+  rsync -r --delete --quiet $TRAVIS_BUILD_DIR/public travis@206.189.245.160:gnib-visa-app/public
+else
+  echo "Not deploying, since this branch isn't master."
+fi
