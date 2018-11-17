11597e0fce06d7f86292ab23d17922446381b434
Harshad Ranganathan <RanganathanH@DNB.com>
2018-11-17 19:59:51 +0000

    Fix deploy issues

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -xe
 if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
+  npm run build
   rsync -r --delete --quiet $TRAVIS_BUILD_DIR/public travis@206.189.245.160:gnib-visa-app/public
 else
   echo "Not deploying, since this branch isn't master."
