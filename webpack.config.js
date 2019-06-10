83fea830604d67162b2d4e062d4d460d01e141c6
RanganathanH <ranganathanh@dnb.com>
2019-06-10 21:47:03 +0100

    Upgrade babel and fix security issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const srcPath = path.join(__dirname, 'src');
-const buildPath = path.join(__dirname, 'public');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcPath = path.join(__dirname, "src");
+const buildPath = path.join(__dirname, "public");
 
-module.exports = () => {    
-    return {
-        entry: ['babel-polyfill', path.join(srcPath, 'index.js')],
-        output: {
-            path: buildPath,
-            filename: 'bundle.js'
+module.exports = () => {
+  return {
+    entry: ["@babel/polyfill", path.join(srcPath, "index.js")],
+    output: {
+      path: buildPath,
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [{ loader: "babel-loader" }]
         },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: [{ loader: 'babel-loader' }]
-                }, 
-                {
-                    test: /\.css$/,
-                    use: [
-                        { loader: 'style-loader' },
-                        { loader: 'css-loader' },
-                        { loader: 'postcss-loader' }
-                    ]
-                }
-            ]
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.join(__dirname, 'index.ejs'),
-                path: buildPath,
-                excludeChunks: ['base'],
-                filename: 'index.html',
-                minify: {
-                    collapseWhitespace: true,
-                    collapseInlineTagWhitespace: true,
-                    removeComments: true,
-                    removeRedundantAttributes: true
-                }
-            }),
-            new CopyPlugin([
-                { from: path.join(srcPath, 'pwa'), to: buildPath }
-            ])
-        ]
-    };
+        {
+          test: /\.css$/,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" },
+            { loader: "postcss-loader" }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "index.ejs"),
+        path: buildPath,
+        excludeChunks: ["base"],
+        filename: "index.html",
+        minify: {
+          collapseWhitespace: true,
+          collapseInlineTagWhitespace: true,
+          removeComments: true,
+          removeRedundantAttributes: true
+        }
+      }),
+      new CopyPlugin([{ from: path.join(srcPath, "pwa"), to: buildPath }])
+    ]
+  };
 };
